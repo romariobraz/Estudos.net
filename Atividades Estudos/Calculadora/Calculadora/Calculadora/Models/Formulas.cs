41bf5f3e6e18c7e3a6264eb353ba9500bb84270a
Romario <romarioinfobraz@gmail.com>
2025-08-19 10:41:34 -0300

    Update
    Mais aulas sobre math, e operadores lógicos.

---
+++ Atividades Estudos/Calculadora/Calculadora/Calculadora/Models/Formulas.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operacoes.Models
+{
+    public class Formulas
+    {
+        public void Somar(int x, int y)
+            {
+                Console.WriteLine($"{x} + {y} = {x + y}");
+            }
+
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"{x} - {y} = {x - y}");
+        }
+
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"{x} X {y} = {x * y}");
+        }
+
+        public void Dividir(int x, int y)
+        {
+            Console.WriteLine($"{x} / {y} = {x / y}");
+        }
+
+        public void Potencia(int x, int y)
+        {
+            double pot = Math.Pow(x, y);
+            Console.WriteLine($"{x} ^ {y} = {pot}");
+            
+        }
+
+        public void Seno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"O Seno de {angulo} = {Math.Round(seno, 4)}");
+        }
+
+        public void Coseno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double coseno = Math.Cos(radiano);
+            Console.WriteLine($"O Coseno de {angulo} = {Math.Round(coseno, 4)}");
+        }
+
+        public void Tangente(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double tan = Math.Tan(radiano);
+            Console.WriteLine($"O Tangente de {angulo} = {Math.Round(tan, 4)}");
+        }
+
+        public void RaizQ( double x)
+        {
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine($"A raiz quadrada de {x} é {raiz}");
+        }
+
+        public void Imc(double peso, double altura, double valorImc)
+        {
+            Console.WriteLine("Informe o peso em KG: ");
+            peso = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe a altura: ");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+            valorImc = peso / (altura * altura);
+
+            Console.WriteLine($"Seu IMC é: {valorImc}");
+
+            if( valorImc < 18.5)
+            {
+                Console.WriteLine("Você está abaixo do peso");
+            }
+            else if( valorImc >= 18.5 &&  valorImc < 24.9)
+            {
+                Console.WriteLine("Seu peso está normal");
+            }
+            else if (valorImc >= 25 && valorImc < 29.9)
+            {
+                Console.WriteLine("Você está com sobrepeso");
+            }
+            else if (valorImc >= 30 && valorImc < 39.9)
+            {
+                Console.WriteLine("Você está com obesidade I");
+            }
+            else
+            {
+                Console.WriteLine("Você está com obesidade mórbida, cuide de sua saúde.");
+            }
+
+        }
+
+    }
+}
--- Atividades Estudos/MultiplicandoMatrizes/Operadores Condicionais/Operadores Condicionais/Program.cs
+++ Atividades Estudos/MultiplicandoMatrizes/Operadores Condicionais/Operadores Condicionais/Program.cs
@@ -22,6 +22,7 @@ namespace Operadores_Condicionais
 
             //usando um booleano para validar a venda
             bool valida = qEstoque >= qCompra && qCompra > 0;
+            //Meu estoque precisa ser maior que o valor da compra, e o valor da compra precisa ser maior que 0
             //Não existe uma venda com 0 itens
 
             //Agora vem a nossa estrutura de decisão
@@ -32,7 +33,7 @@ namespace Operadores_Condicionais
             }
             //Caso o if não seja atendido
             else if (valida == false)
-            //else if é um dos meios de se aninhar um if
+            //else if é um dos meios de se aninhar um if, podemos usar quantos else if quisermos, um atrás do outro
             {
                 Console.WriteLine("Valor para compra precisa ser maior do que 0");
             }
@@ -42,6 +43,25 @@ namespace Operadores_Condicionais
             }
             //Posso usar booleanos também para conferir se a condição foi ou não atendida
             Console.WriteLine($"É possível realizar a venda? "+ valida);
+            */
+            #endregion
+
+            #region Aninhando If else
+            /*
+            int n1 = 9;
+            if (n1 > 5)
+            {
+                Console.WriteLine("O número é maior que 5");
+
+                if (n1 % 2 == 0) 
+                { 
+                    Console.WriteLine("O número é PAR");
+                }
+                else
+                {
+                    Console.WriteLine("O número é impar");
+                }
+            }
             */
             #endregion
 
@@ -68,6 +88,28 @@ namespace Operadores_Condicionais
             */
             #endregion
 
+            #region Operador Ternário
+            /*
+            int n2 = 4;
+            string mensagem = "";
+            
+            if (n2 > 5)
+            {
+                mensagem = " maior do que 5";
+            }
+            else
+            {
+                mensagem = " menor do que 5";
+            }
+            
+            //Para realizar esta mesma operação acima com um operador ternário, podemos fazer:
+            mensagem = n2 > 5 ? "Maior que 5" : "Menor que 5";
+            //O operador : é o chamado operador ternário, já o ? está condicionando a condição ao operador
+
+            Console.WriteLine($"O número é {mensagem}");
+            
+            */
+            #endregion
 
         }
     }
