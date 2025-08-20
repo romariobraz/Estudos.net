d747fe5a42eee0721fab03216b035bd830078bc9
Romario <romarioinfobraz@gmail.com>
2025-08-20 14:32:00 -0300

    Orientado a objetos
    Comecei a usar a orientação a objetos, com métodos, usei get e set. além de criar uma exceção.

---
+++ 6.Atributos das Classes/Cadastro/Cadastro/Metodos/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro.Metodos
+{
+    internal class Pessoa
+    {
+        private string _nome;
+        public string nome
+        {
+            get { return _nome.ToUpper(); }
+            set { 
+                if(value == "") {
+                    throw new ArgumentException("Nome vazio!");
+                }
+                _nome = value;
+                }
+        }
+        public int idade { get; set; }
+    }
+}
---
+++ 6.Atributos das Classes/Cadastro/Cadastro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cadastro.Metodos; // 1. Chama o namespace
+
+namespace Cadastro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa(); // 2 Instância
+            //Agora pode sentar o aço.
+            pessoa.nome = "";
+            pessoa.idade = 31;
+
+            Console.WriteLine($"Meu nome é: {pessoa.nome}");
+            Console.WriteLine($"Minha idade é: {pessoa.idade}");
+        }
+    }
+}
