32c8282a4fcf048d0152c38687be967d428d9569
Romario <romarioinfobraz@gmail.com>
2025-08-21 10:34:20 -0300

    Aula de construtores
    Aula completa, declaração dos construtores e a sobrecarga deles.

--- 7.This/This/This/Program.cs
+++ 7.This/This/This/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -38,6 +39,27 @@ namespace This
             conta -= m.Dividir;//exclui essa com uma operação
             conta -= m.Subtrair;//exclui essa com uma operação
             conta(20, 4);//novos valores para minha variável.
+
+            //Agora vamos para a aula de construtores
+
+            Pessoa p1 = new Pessoa();
+
+            Console.WriteLine("Meu nome é: "+ p1.nome);
+            Console.WriteLine("Meu sobrenome é: "+ p1.sobrenome);
+            Console.WriteLine("Eu nasci do ano de: "+ p1.anoNascimento);
+            Console.WriteLine($"Atualmente tenho {p1.idade} anos.\n");
+
+            Pessoa p2 = new Pessoa("Amanda", "Almeida", 2007);
+            Console.WriteLine("Meu nome é: " + p2.nome);
+            Console.WriteLine("Meu sobrenome é: " + p2.sobrenome);
+            Console.WriteLine("Eu nasci do ano de: " + p2.anoNascimento);
+            Console.WriteLine($"Atualmente tenho {p2.idade} anos.\n");
+
+            Pessoa p3 = new Pessoa("Ricardo", "Santos");
+            Console.WriteLine("Meu nome é: " + p3.nome);
+            Console.WriteLine("Meu sobrenome é: " + p3.sobrenome);
+            Console.WriteLine("Eu nasci do ano de: " + p3.anoNascimento);
+            Console.WriteLine($"Atualmente tenho {p3.idade} anos.\n");
 
             Console.ReadKey();
         }
