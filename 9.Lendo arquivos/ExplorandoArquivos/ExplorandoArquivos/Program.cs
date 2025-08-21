6a24b5ce06423f830b4b5e585d7302ddbce8d0dd
Romario <romarioinfobraz@gmail.com>
2025-08-21 17:47:06 -0300

    Aulas sobre manuseio de arquivos
    Abrir arquivo.

---
+++ 9.Lendo arquivos/ExplorandoArquivos/ExplorandoArquivos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplorandoArquivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] linhas = File.ReadAllLines("D:/Arquivos/GitHub/Estudos.net/9.Lendo arquivos/ExplorandoArquivos/ExplorandoArquivos/Arquivos/Dados.txt");
+
+            foreach (string linha in linhas)
+            {
+                Console.WriteLine(linha);
+
+            }
+        }
+    }
+}
