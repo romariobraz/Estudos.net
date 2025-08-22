6c1b07300693d126d18ed08aec577d35daca26ed
Romario <romarioinfobraz@gmail.com>
2025-08-22 08:42:28 -0300

    Tratativa de exceção
    Try, catch, finaly.

--- 10.Exceções/ExplorandoArquivos/ExplorandoArquivos/Program.cs
+++ 10.Exceções/ExplorandoArquivos/ExplorandoArquivos/Program.cs
@@ -23,10 +23,22 @@ namespace ExplorandoArquivos
 
                 }
             }
+            catch(FileNotFoundException ex) //Erro de arquivo não encontrado
+            {
+                Console.WriteLine("Ocorreu um erro, arquivo não encontrado!");
+            }
+            catch (DirectoryNotFoundException ex) //Erro de diretório não encontrado
+            {
+                Console.WriteLine("Ocorreu um erro, diretório não encontrado!");
+            }
 
-            catch(Exception ex) //estamos capturando o errro da excessão
+            catch (Exception ex) //estamos capturando o errro da excessão
             { 
                       Console.WriteLine($"Ocorreu uma exceção genérica: {ex.Message}"); //e escrevendo ela
+            }
+            finally // finaly é sempre executado após o catch, mesmo que ocorra alguma excessão.
+            {
+                Console.WriteLine("Finalizamos aqui");
             }
 
 
