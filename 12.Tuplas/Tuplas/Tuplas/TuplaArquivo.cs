57f16dd31025a4edba796cdd22ea93657c9ad3eb
Romario <romarioinfobraz@gmail.com>
2025-08-22 10:32:48 -0300

    Tuplas
    Aula 1 e 2 sobre tuplar, criação, definição e tuplas em métodos.

---
+++ 12.Tuplas/Tuplas/Tuplas/TuplaArquivo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuplas
+{
+    internal class TuplaArquivo
+    {
+        public (bool Sucesso, string[] LinhasArquivo, int QuantidadeLinhas) LerArquivo(string caminho)
+        {
+            try
+            {
+                string[] linhas = File.ReadAllLines(caminho);
+                return (true, linhas, linhas.Count);
+            }
+            catch (Exception ex)
+            {
+                return(false, new string[], 0)
+            }
+        }
+    }
+}
