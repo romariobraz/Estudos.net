2ce074a8c0d5148186dc6fd273f778997b161b77
Romario <romarioinfobraz@gmail.com>
2025-08-22 07:21:06 -0300

    Tratamento de exceções
    1 aula

---
+++ 10.Exceções/ExplorandoArquivos/ExplorandoArquivos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplorandoArquivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try { //Tenta
+
+                string[] linhas = File.ReadAllLines("D:\\Arquivos\\GitHub\\Estudos.net\\10.Exceções\\ExplorandoArquivos\\ExplorandoArquivos\\Arquivos\\Dados.txt");
+                
+                
+                foreach (string linha in linhas)
+                {
+                    Console.WriteLine(linha);
+
+                }
+            }
+
+            catch(Exception ex) //estamos capturando o errro da excessão
+            { 
+                      Console.WriteLine($"Ocorreu uma exceção genérica: {ex.Message}"); //e escrevendo ela
+            }
+
+
+        }
+    }
+}
