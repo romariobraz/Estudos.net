c2f2559d690b06815492c233626e6cc5f7bae29c
Romario <romarioinfobraz@gmail.com>
2025-08-28 14:01:43 -0300

    Atualizado.
    Finalização.

--- 18.Entity/Entity/obj/Debug/net9.0/Entity.AssemblyInfo.cs
+++ 18.Entity/Entity/obj/Debug/net9.0/Entity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62ea6d780c5f3bb984c9a779f538e798b2738e50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1282339fe53892587eafdd78ec7cbff741fe8a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
