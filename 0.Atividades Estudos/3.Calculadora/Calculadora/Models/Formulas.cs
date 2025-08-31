4321083e9448aa6b1d03bf4ae8f398b031e90185
Romario <romarioinfobraz@gmail.com>
2025-08-31 01:59:55 -0300

    Projeto Calculadora
    Transformei uma calculadora básica de prompt, em uma calculadora científica, com seno, coseno, tangente, transformação de temperaturas.
    Ela possui interface gráfica, e salva e recuper seu histórico em .json.

--- 0.Atividades Estudos/3.Calculadora/Calculadora/Models/Formulas.cs
+++ 0.Atividades Estudos/3.Calculadora/Calculadora/Models/Formulas.cs
@@ -3,153 +3,162 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using Janela.MainWindow;
 
 namespace Operacoes.Models
 {
     public class Formulas
     {
-        public void Somar(double x, double y)
-            {
-            Console.WriteLine("Digite o valor do primeiro número: ");
-            x = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Digite o valor do segundo número");
-            y = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine($"{x} + {y} = {x + y}");
-            }
-
-        public void Subtrair(double x, double y)
+        #region Calculos Simples
+        //Primeiro vamos para os calculos mais simples + - * / e %
+        public double Somar(double x, double y)
         {
-            Console.WriteLine("Digite o valor do primeiro número: ");
-            x = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Digite o valor do segundo número");
-            y = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine($"{x} - {y} = {x - y}");
+            return x + y;
         }
 
-        public void Multiplicar(double x, double y)
+        public double Subtrair(double x, double y)
         {
-            Console.WriteLine("Digite o valor do primeiro número: ");
-            x = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Digite o valor do segundo número");
-            y = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine($"{x} X {y} = {x * y}");
+            return x - y;
         }
 
-        public void Dividir(double x, double y)
+        public double Multiplicar(double x, double y)
         {
-            Console.WriteLine("Digite o valor do primeiro número: ");
-            x = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Digite o valor do segundo número");
-            y = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine($"{x} / {y} = {x / y}");
+            return x * y;
         }
 
-        public void Potencia(double x, double y)
+        public double Dividir(double x, double y)
         {
-            Console.WriteLine("Digite o valor do primeiro número: ");
-            x = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Digite o valor do segundo número");
-            y = Convert.ToDouble(Console.ReadLine());
-            double pot = Math.Pow(x, y);
-            Console.WriteLine($"{x} ^ {y} = {pot}");
-            
+            return x / y;
+        }
+        public double Resto(double x, double y)
+        {
+            return x % y;
+        }
+        #endregion
+
+        #region %
+        public double Porcentagem(double porcentagem, double numero)
+        { 
+            return (porcentagem / 100) * numero;
         }
 
-        public void Seno(double angulo)
+
+        #endregion
+
+        #region Algebra
+        //Vamos para a algebra 
+        public double Potencia(double x, double y)
         {
-            Console.WriteLine("Digite o valor do angulo: ");
-            angulo = Convert.ToDouble(Console.ReadLine());
+            return Math.Round(Math.Pow(x, y), 4);
+        }
+        public double RaizQ(double x)
+        {
+            return Math.Round(Math.Sqrt(x), 4);
+        }
+        #endregion
+
+        #region Trigonometria
+        //Agora para trigonometria Seno, coseno e tangente
+        public double Seno(double angulo)
+        {
             double radiano = angulo * Math.PI / 180;
             double seno = Math.Sin(radiano);
-            Console.WriteLine($"O Seno de {angulo} = {Math.Round(seno, 4)}");
+            return Math.Round(seno, 4);
         }
 
-        public void Coseno(double angulo)
+        public double Cosseno(double angulo)
         {
-            Console.WriteLine("Digite o valor do angulo: ");
-            angulo = Convert.ToDouble(Console.ReadLine());
             double radiano = angulo * Math.PI / 180;
             double coseno = Math.Cos(radiano);
-            Console.WriteLine($"O Coseno de {angulo} = {Math.Round(coseno, 4)}");
+            return Math.Round(coseno, 4);
         }
 
-        public void Tangente(double angulo)
+        public double Tangente(double angulo)
         {
-            Console.WriteLine("Digite o valor do angulo: ");
-            angulo = Convert.ToDouble(Console.ReadLine());
             double radiano = angulo * Math.PI / 180;
-            double tan = Math.Tan(radiano);
-            Console.WriteLine($"O Tangente de {angulo} = {Math.Round(tan, 4)}");
+            double tangente = Math.Tan(radiano);
+            return Math.Round(tangente, 4);
         }
+        #endregion
 
-        public void RaizQ( double x)
+        #region Extras (IMC e Fobacci)
+        //Estes dois são alguns extras de aulas passadas
+        public (double valor, string classificacao) Imc(double peso, double altura)
         {
-            Console.WriteLine("Digite o valor do número: ");
-            x = Convert.ToDouble(Console.ReadLine());
-            double raiz = Math.Sqrt(x);
-            Console.WriteLine($"A raiz quadrada de {x} é {raiz}");
+            double valorImc = peso / (altura * altura);
+            string classificacao;
+
+            if (valorImc < 18.5) classificacao = "Abaixo do peso";
+            else if (valorImc < 24.9) classificacao = "Peso normal";
+            else if (valorImc < 29.9) classificacao = "Sobrepeso";
+            else if (valorImc < 39.9) classificacao = "Obesidade I";
+            else classificacao = "Obesidade mórbida";
+
+            return (Math.Round(valorImc, 2), classificacao);
         }
 
-        public void Imc(double peso, double altura, double valorImc)
+        public List<double> Fibonacci(int n)
         {
-            Console.WriteLine("Informe o peso em KG: ");
-            peso = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Informe a altura: ");
-            altura = Convert.ToDouble(Console.ReadLine());
-
-            valorImc = peso / (altura * altura);
-
-            Console.WriteLine($"Seu IMC é: {valorImc}");
-
-            if( valorImc < 18.5)
+            List<double> seq = new List<double> { 0, 1 };
+            for (int i = 2; i <= n; i++)
             {
-                Console.WriteLine("Você está abaixo do peso");
+                seq.Add(seq[i - 1] + seq[i - 2]);
             }
-            else if( valorImc >= 18.5 &&  valorImc < 24.9)
-            {
-                Console.WriteLine("Seu peso está normal");
-            }
-            else if (valorImc >= 25 && valorImc < 29.9)
-            {
-                Console.WriteLine("Você está com sobrepeso");
-            }
-            else if (valorImc >= 30 && valorImc < 39.9)
-            {
-                Console.WriteLine("Você está com obesidade I");
-            }
-            else
-            {
-                Console.WriteLine("Você está com obesidade mórbida, cuide de sua saúde.");
-            }
-
+            return seq;
         }
+        #endregion
 
-        public void Fibonacci(int a, int b, int c)
+        #region Geometria
+        //Agora vamos para formulas geométricas
+        public double Raio(double area)
         {
-            a = 0; b = 1; c = 0;
-
-            Console.WriteLine("Quantos valores você deseja imprimir?");
-            int valores = int.Parse(Console.ReadLine());
-
-            Console.WriteLine($"Sequencia de Fibonacci com {valores} valores");
-
-            for (int i = 0; i < valores; i++)
-            {
-                if ( i < valores - 1)
-                {
-                    Console.Write(a + ",");
-                }
-                else
-                {
-                    Console.Write(a + ".");
-                }
-                
-                c = a + b;
-                a = b;
-                b = c;
-
-            }
+            if (area < 0)
+                throw new ArgumentException("Área não pode ser negativa!");
+            return Math.Sqrt(area / Math.PI);
         }
+        public double AreaQuadrado(double lado)
+        {
 
+            return Math.Round(Math.Pow(lado, 2), 4);
+        }
+        public double AreaRetangulo(double baseRet, double altura)
+        {
+            return Math.Round(baseRet * altura, 4);
+        }
+        public double AreaCirculo(double raio)
+        {
+            return Math.Round(Math.PI * Math.Pow(raio, 2), 4);
+        }
+        #endregion
+
+        #region Temperaturas
+        //Agora vamos trabalhar com conversão de temperaturas
+        //C = Celsius, K = Kevin F = Fahrenheit
+        public double CF(double c)
+        {
+            return Math.Round((c * 9.0 / 5.0) + 32, 2);
+        }
+        public double CK(double c)
+        {
+            return Math.Round(c + 273.15, 2);
+        }
+        public double KF(double k)
+        {
+            return Math.Round((k - 273.15) * 9.0 / 5.0 + 32, 2);
+        }
+        public double KC(double k)
+        {
+            return Math.Round(k - 273.15, 2);
+        }
+        public double FK(double f)
+        {
+            return Math.Round((f - 32) * 5.0 / 9.0 + 273.15, 2);
+        }
+        public double FC(double f)
+        {
+            return Math.Round((f - 32) * 5.0 / 9.0, 2);
+        }
+        #endregion
     }
 }
